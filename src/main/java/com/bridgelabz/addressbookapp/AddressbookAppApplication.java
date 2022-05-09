a35b77ac584945aad054a5aef6b7d8e8de62b11b
KarthikBenki <karthikmc007@gmail.com>
2022-05-09 21:04:52 +0530

    Annotation @Slf4j at Class Level, Logging is activated Logging Levels, Files and Patterns are handled in application.properties

--- src/main/java/com/bridgelabz/addressbookapp/AddressbookAppApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressbookAppApplication.java
@@ -1,14 +1,20 @@
 package com.bridgelabz.addressbookapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class AddressbookAppApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Addressbook App");
-		SpringApplication.run(AddressbookAppApplication.class, args);
+		ApplicationContext context = SpringApplication.run(AddressbookAppApplication.class, args);
+		log.info("Addressbook App Started in {} Environment",context.getEnvironment().getProperty("environment"));
+		log.info("Addressbook App started");
 	}
 
 }
